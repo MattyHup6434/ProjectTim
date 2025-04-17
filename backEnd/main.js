492b90651f427cbe753687d13524d31ca6ec9cfc
vrealz <verawich2211@gmail.com>
2025-04-17 17:02:02 +0700

    ggez

--- backEnd/main.js
+++ backEnd/main.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT ;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
